60af8be66a9619e0dc3834ad2d53f592dd4bf9b4
GeeksforGeeks <prajyothere@gmail.com>
2023-12-18 20:59:25 +0530

    style: sidebar add button pdated

--- src/components/sidebar/SideBar.jsx
+++ src/components/sidebar/SideBar.jsx
@@ -9,18 +9,24 @@ import { useDispatch, useSelector } from "react-redux";
 import { store } from "../../redux/store";
 import categoryReducer from "../../redux/reducers";
 
-const SideBar = ({ handleCategoryClick }) => {
-  const [categorychanged, setcategorychanged] = useState(false);
-  const [toggleMenu, setToggleMenu] = useState(false);
+let colors = {
+  "1" : "#B38BFA",
+  "2" : "#FF79F2",
+  "3" : "#43E6FC",
+  "4" : "#F19576",
+  "5" : "#0047FF",
+  "6" : "#6691FF",
+}
 
+
+const SideBar = ({ handleCategoryClick }) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [groups, setGroups] = useState([]);
+  const [group, setGroup] = useState("");
   const [selectedLink, dispatchSideBarLink] = useReducer(categoryReducer, "");
-  // const handleLinkClick = (link) => {
-  //   setSelectedLink(link);
-  // };
 
   const buttonClickHandler = (newCategory) => {
     handleCategoryClick(newCategory);
-    // setSelectedLink(newCategory);
     dispatchSideBarLink({ type: "UPDATE_SELECTED_LINK", payload: newCategory });
     store.dispatch({
       type: "CHANGE_CATEGORY",
@@ -28,20 +34,60 @@ const SideBar = ({ handleCategoryClick }) => {
     });
   };
 
+  const handleAddGrp = (e) => {
+    e.preventDefault();
+    setGroups([...groups, group]);
+    setGroup("");
+  };
+
+  const modal = document.querySelector("#modal");
+  const openModal = document.querySelector("#openModal");
+
+  const closeModal = document.querySelector("#closeModal");
+
+  if (modal) {
+    openModal && openModal.addEventListener("click", () => modal.showModal());
+
+    closeModal && closeModal.addEventListener("click", () => modal.close());
+  }
+
+  const handleInputChange = (e) => {
+    const { value } = e.target;
+    console.log("this is e.target.value : " + e.target.value);
+    setGroup(value);
+  };
+
   return (
     <div className="app_sidebar">
       <div className="app_sidebar_title">Pocket Notes</div>
       <div className="app_sidebar-links_container">
-        <a href="#group1" onClick={() => buttonClickHandler("Group1")}>
-          <p>Group1</p>
-        </a>
-        <a href="#group2" onClick={() => buttonClickHandler("Group2")}>
-          <p>Group2</p>
-        </a>
-        <a href="#group3" onClick={() => buttonClickHandler("Group3")}>
-          <p>Group3</p>
-        </a>
-        <button onClick={() => buttonClickHandler("Group4")}>Click Me</button>
+        {groups.map((group) => (
+          <a href="#group" onClick={() => buttonClickHandler(group)}>
+            <p>{group}</p>
+          </a>
+        ))}
+        <dialog id="modal">
+          <p>Create New Group</p>
+          <label>Group Name </label>
+          <input
+            type="text"
+            name="newgrp"
+            id="newgrpinput"
+            value={group}
+            onChange={handleInputChange}
+          />
+          <br />
+          <label>Choose Color :</label>
+          <div onClick={() => buttonClickHandler(color)} style={{borderRadius:"50%"}}>
+          </div>
+          <br />
+          <button id="closeModal" onClick={handleAddGrp} className="createbutton">
+            Create
+          </button>
+        </dialog>
+        <button id="openModal" className="addcircle">
+          +
+        </button>
       </div>
     </div>
   );
